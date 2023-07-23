0bc030cfa6b266487d49370323b0dff2110c9cd4
Connor Sheets <connorsheets32@gmail.com>
2023-07-23 13:39:20 -0500

    i have a cool animationg for rectangles

--- FractalCanvas.java
+++ FractalCanvas.java
@@ -1,3 +1,4 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -7,22 +8,70 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class FractalCanvas extends Application {
+    private int canvasWidth = 800;
+    private int canvasHeight = 600;
+    private int rectHeight = 30;
+    private int rectWidth = 50;
+    private Canvas canvas;
+    private GraphicsContext gc;
+    private int numRectsX;
+    private int numRectsY;
+    private int currentRow = 0;
+    private int currentCol = 0;
+
     @Override
     public void start(Stage primaryStage) {
-        Canvas canvas = new Canvas(400, 400);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+        canvas = new Canvas(canvasWidth, canvasHeight);
+        gc = canvas.getGraphicsContext2D();
+        numRectsX = canvasWidth / rectWidth;
+        numRectsY = canvasHeight / rectHeight;
+
         
-        // Draw the fractals or other visualizations on the canvas here using GraphicsContext methods
+        AnimationTimer animationTimer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                drawRectangles();
+                // Stop the animation when all rectangles are drawn
+                if (currentRow >= numRectsY && currentCol >= numRectsX) {
+                    stop();
+                }
+            }
+        };
+        animationTimer.start();
+
 
         StackPane root = new StackPane(canvas);
         Scene scene = new Scene(root, 400, 400, Color.WHITE);
-        
+
         primaryStage.setScene(scene);
         primaryStage.setTitle("JavaFX Fractal Visualization");
         primaryStage.show();
+    }
+
+    private void drawRectangles() {
+        if (currentRow < numRectsY) {
+            double x = currentCol * rectWidth;
+            double y = currentRow * rectHeight;
+            gc.setFill(Color.BLUE);
+            gc.fillRect(x, y, rectWidth, rectHeight);
+
+            currentCol++;
+            if (currentCol >= numRectsX) {
+                currentCol = 0;
+                currentRow++;
+            }
+
+            
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 }
+
